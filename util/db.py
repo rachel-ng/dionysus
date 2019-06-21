d1a4be600786003e91b5b1c6ebc1695bbffa5110
Rachel Ng <rng@stuy.edu>
2019-06-21 19:12:00 -0400

    db

--- util/db.py
+++ util/db.py
@@ -1,32 +1,46 @@
 import os, sqlite3
-from passlib.hash import sha256_crypt
+from passlib.hash import sha256_crypt # password hashing -- sha256_crypt.hash(hash_this)
 
 from util import config
 
 config.create_table()
 
 def add_user(username, password):
+    '''
+    was this user added to TABLE users? 
+    '''
     db, c = config.start_db()
     data = c.execute("SELECT * FROM users;")
-    for row in data:
-        if username == row[1]:
+    for row in data: # is this username available
+        if username == row[1]: 
+            config.end_db(db)
             return False
     command = "INSERT INTO users (username,password) VALUES (?,?);"
     c.execute(command,(username,sha256_crypt.hash(password)))
     config.end_db(db)
     return True
 
+# rEwRiTe THiS adsfkhjkadsfhkadhskl 
 def auth_user(username, password):
     db, c = config.start_db()
-    data = c.execute("SELECT * FROM users")
+    data = c.execute("SELECT * FROM users;")
+    #command = "SELECT password FROM users WHERE username = ?;"
+    #c.execute(command,(username,))
+    #pw = c.fetchone()[0]
+    #if pw == sha256_crypt.verify(password, row[1]): 
+    #   config.end_db(db)
+    #   return True
     for row in data:
         if row[0] == username and sha256_crypt.verify(password, row[1]):
-            db.close()
+            config.end_db(db)
             return True
     config.end_db(db)
     return False
 
 def registered(username):
+    '''
+    is this username available? 
+    '''
     db, c = config.start_db()
     command = "SELECT * FROM users;"
     c.execute(command)
@@ -34,11 +48,15 @@ def registered(username):
     print(data)
     for row in data:
         if username == row[0]:
+            config.end_db(db)
             return False
     config.end_db(db)
     return True
 
 def all_users():
+    '''
+    returns a dictionary with usernames + hashes passwords
+    '''
     db, c = config.start_db()
     command = "SELECT username, password FROM users;"
     c.execute(command)
