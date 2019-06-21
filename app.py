82ae74a88cc812a26e8ba2afbd1393a4effdb407
rachel-ng <rng@stuy.edu>
2019-06-21 17:31:54 -0400

    .

---
+++ app.py
@@ -0,0 +1,94 @@
+import os, random
+from flask import Flask, render_template, request, session, url_for, redirect, flash
+
+from util import config, db
+
+app = Flask(__name__, static_url_path='/static')
+
+app.secret_key = os.urandom(32)
+
+config.create_table()
+
+@app.route('/')
+def root():
+    guest = 'user' not in session
+    user = None
+    if not guest: user = session['user']
+    return render_template("base.html", guest = guest, user = user)
+
+@app.route('/login')
+def login():
+    '''
+    login page
+    '''
+    guest = 'user' not in session
+    if not guest:
+        return redirect(url_for('user'))
+    return render_template('login.html', guest = guest)
+
+@app.route('/login_auth', methods = ['POST'])
+def login_auth():
+    '''
+    login authorization
+    '''
+    username = request.form['username']
+    password = request.form['password']
+    if db.auth_user(username, password):
+        session['user'] = username
+        flash("You have logged in")
+        return redirect('/')
+    else:
+        flash("Invalid username and password combination")
+        return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    '''
+    signup page
+    '''
+    guest = 'user' not in session
+    return render_template("signup.html", guest = guest)
+
+@app.route('/signup_auth', methods = ['POST'])
+def register_auth():
+    '''
+    signup authorization
+    '''
+    username = request.form['username']
+    password = request.form['password']
+    retyped_pass = request.form['repass']
+    print(db.registered(username))
+    if username == "":
+        flash("Enter a username")
+        return redirect(url_for('signup'))
+    if not db.registered(username):
+        flash("Choose a different username")
+        return redirect(url_for('signup'))
+    elif password == "":
+        flash("Enter a password")
+        return redirect(url_for('signup'))
+    elif password != retyped_pass:
+        flash("Passwords do not match")
+        return redirect(url_for('signup'))
+    else:
+        if db.add_user(username, password):
+            flash("You have successfully registered")
+        else:
+            flash("This username is already in use")
+            return redirect(url_for('signup'))
+    db.auth_user(username, password)
+    session['user'] = username
+    return redirect('/')
+
+@app.route('/logout')
+def logout():
+    '''
+    logout
+    '''
+    if 'user' in session:
+        session.pop('user')
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
